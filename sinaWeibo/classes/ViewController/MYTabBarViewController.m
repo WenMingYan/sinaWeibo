454062ecb307a23fdfd3f7e0362e429a7ff799c1
wmy <mingyan.wmy@alibaba-inc.com>
2015-09-06 14:31:59 +0800

    commit 添加tabor的image和title

--- sinaWeibo/classes/ViewController/MYTabBarViewController.m
+++ sinaWeibo/classes/ViewController/MYTabBarViewController.m
@@ -21,18 +21,30 @@
     UIViewController *home = [[UIViewController alloc] init];
     home.view.backgroundColor = [UIColor redColor];
     [self addChildViewController:home];
+    home.title = @"首页";
+    home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
+    home.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_home_selected"];
     //--消息
     UIViewController *message = [[UIViewController alloc] init];
     message.view.backgroundColor = [UIColor blackColor];
     [self addChildViewController:message];
+    message.title = @"消息";
+    message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
+    message.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_message_center_selected"];
     //--发现
     UIViewController *find = [[UIViewController alloc] init];
     find.view.backgroundColor = [UIColor blueColor];
     [self addChildViewController:find];
+    find.title = @"发现";
+    find.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
+    find.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_discover_selected"];
     //--我
     UIViewController *me = [[UIViewController alloc] init];
     me.view.backgroundColor = [UIColor purpleColor];
     [self addChildViewController:me];
+    me.title = @"我";
+    me.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
+    me.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_profile_selected"];
 
 }
 
