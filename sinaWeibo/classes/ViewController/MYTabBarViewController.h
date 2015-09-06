662e87750ceede215bbaf65b6d242a787bb1990a
wmy <mingyan.wmy@alibaba-inc.com>
2015-09-06 14:17:23 +0800

    commit 第一天项目搭建

--- sinaWeibo/classes/ViewController/MYTabBarViewController.h
+++ sinaWeibo/classes/ViewController/MYTabBarViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MYTabBarViewController.h
 //  sinaWeibo
 //
 //  Created by 温明妍 on 15/9/6.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MYTabBarViewController : UITabBarController
 
 @end
-
---
+++ sinaWeibo/classes/ViewController/MYTabBarViewController.m
@@ -0,0 +1,37 @@
+//
+//  MYTabBarViewController.m
+//  sinaWeibo
+//
+//  Created by 温明妍 on 15/9/6.
+//  Copyright (c) 2015年 wmy. All rights reserved.
+//
+
+#import "MYTabBarViewController.h"
+
+@interface MYTabBarViewController ()
+
+@end
+
+@implementation MYTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
